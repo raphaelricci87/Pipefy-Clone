b17604df3b7091af9f6cd24e05f4424c7962b383
Santos <raphael.b.santos@capgemini.com>
2020-01-30 14:11:16 -0300

    projeto

---
+++ src/components/Header/styles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    height: 88px;
+    padding: 0 30px;
+    background: #5DBCD2;
+    color: #FFF;
+    display: flex;
+    align-items: center;
+`;
